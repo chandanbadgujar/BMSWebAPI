b8bae3c4ed8943425e8d76cbc0dd50df3b83abbc
ChandanBadgujar <chandanbadgujar@hotmail.com>
2021-10-14 19:22:45 +0530

    register method unit test

---
+++ BMS.Tests/BMS.Tests/Models/RegisterModelMock.cs
@@ -0,0 +1,32 @@
+﻿using BMSWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BMS.Tests.Models
+{
+    public class RegisterModelMock
+    {
+        public RegisterModel getRegisterModel()
+        {
+            return new RegisterModel() {
+                AccountType = 1,
+                Address = "dummy",
+                CitizenshipType = 1,
+                ContactNo = "9888555885",
+                Country = 1,
+                State = 1,
+                Dob = DateTime.Now.AddDays(-1000),
+                Email = "ch@ch.com",
+                Gender = 1,
+                GuardianName = "gn dummy",
+                InitialDeposit = 34343,
+                Name="some name",
+                Username ="uname",
+                Password="password",
+                GuardianType=2,
+                MaritalStatus=1
+            };
+        }
+    }
+}
--- BMS.Tests/BMS.Tests/UserServiceUnitTests.cs
+++ BMS.Tests/BMS.Tests/UserServiceUnitTests.cs
@@ -1,4 +1,6 @@
-﻿using BMSWebAPI.Entities;
+﻿using BMS.Tests.Models;
+using BMSWebAPI.Entities;
+using BMSWebAPI.Models;
 using BMSWebAPI.Services;
 using NUnit.Framework;
 using System;
@@ -9,24 +11,51 @@ namespace BMS.Tests
     public class UserServiceUnitTests
     {
         BMSContext _context;
+        RegisterModel _registerModel;
+        RegisterModelMock _registerModelMock;
+        UserService _userService;
         public UserServiceUnitTests(BMSContext context)
         {
             _context = context;
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+            _userService = new UserService(_context);
+            _registerModelMock = new RegisterModelMock();
+            _registerModel = _registerModelMock.getRegisterModel();
         }
 
         [Test]
         public void GetByUsernameSuccess()
         {
             #region Arrange
-            UserService userService = new UserService(_context);
+            //UserService userService = new UserService(_context);
             #endregion
 
             #region Act
-            var user = userService.GetByUsername("test");
+            var user = _userService.GetByUsername("test");
             #endregion
 
             #region Assert
             Assert.AreEqual(typeof(User), user);
+            #endregion
+        }
+
+        [Test]
+        public void RegisterSuccess()
+        {
+            #region Arrange
+            #endregion
+
+            #region Act
+            var result = _userService.Register(_registerModel);
+            #endregion
+
+            #region Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(typeof(int), result);
             #endregion
         }
     }
