7bed6e751bb08445633719d023c3276ae646e642
ChandanBadgujar <chandanbadgujar@hotmail.com>
2021-10-14 10:44:49 +0530

    add unit test project and set projet locations properly

---
+++ BMS.Tests/BMS.Tests/UserServiceUnitTests.cs
@@ -0,0 +1,25 @@
+﻿using BMSWebAPI.Entities;
+using BMSWebAPI.Services;
+using NUnit.Framework;
+using System;
+
+namespace BMS.Tests
+{
+    [TestFixture]
+    public class UserServiceUnitTests
+    {
+        BMSContext _context;
+        public UserServiceUnitTests(BMSContext context)
+        {
+            _context = context;
+        }
+
+        [Test]
+        public void GetByUsernameSuccess()
+        {
+            UserService userService = new UserService(_context);
+            var user = userService.GetByUsername("");
+            Assert.AreEqual(typeof(User), user);
+        }
+    }
+}
